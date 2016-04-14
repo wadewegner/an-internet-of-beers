4d30399d7929d02c879543b01469ceeb898aa2fa
Wade Wegner <wade.wegner@gmail.com>
2016-04-14 11:37:47 -0700

    Updated

--- src/clock/clock.py
+++ src/clock/clock.py
@@ -5,7 +5,7 @@ import os
 
 def job_function():
     url = os.environ.get("TRIGGER_URL")
-    request = Request(url)
+    request = Request(urlheroku)
     response = urlopen(request)
     kittens = response.read()
     print("Hello World")
@@ -15,6 +15,6 @@ def job_function():
 sched = BlockingScheduler()
 
 # Schedule job_function to be called every two hours
-sched.add_job(job_function, 'interval', seconds=2)
+sched.add_job(job_function, 'interval', seconds=3)
 
 sched.start()

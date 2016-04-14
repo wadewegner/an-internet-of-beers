24b73fe0e04bb01fe6b4243f85fc1139eefda87c
Wade Wegner <wade.wegner@gmail.com>
2016-04-14 11:58:19 -0700

    minor tweeks

--- src/clock/clock.py
+++ src/clock/clock.py
@@ -8,14 +8,13 @@ def job_function():
     url = os.environ.get("TRIGGER_URL")
     request = Request(url)
     response = urlopen(request)
-    kittens = response.read()
-    print("Hello World")
+    output = response.read()
     print(url)
-    print(kittens)
+    print(output)
 
 sched = BlockingScheduler()
 
 # Schedule job_function to be called every two hours
-sched.add_job(job_function, 'interval', seconds=3)
+sched.add_job(job_function, 'interval', seconds=60)
 
 sched.start()

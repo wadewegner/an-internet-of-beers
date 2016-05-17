c19d6959d2e75fa3f7f3addd204dba7d998859f7
Wade Wegner <wade.wegner@gmail.com>
2016-05-17 11:28:08 +0100

    Added request npm package

--- src/web/routes.js
+++ src/web/routes.js
@@ -1,4 +1,5 @@
 var config = require('./config/config.js');
+var request = require('request');
 
 function parseCookies (request) {
 	var list = {},
@@ -15,24 +16,25 @@ function parseCookies (request) {
 var token = process.env.FACEBOOK_PAGE_TOKEN;
 
 function sendTextMessage(sender, text) {
-  messageData = {
-    text:text
-  }
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
+	messageData = {
+		text:text
+	}
+
+	request({
+			url: 'https://graph.facebook.com/v2.6/me/messages',
+			qs: {access_token:token},
+			method: 'POST',
+			json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+	if (error) {
+		console.log('Error sending message: ', error);
+	} else if (response.body.error) {
+	console.log('Error: ', response.body.error);
+	}
+	});
 }
 
 module.exports = function(app) {

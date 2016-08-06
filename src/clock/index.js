edd08e51c3111d179285d0975367adc13ae63566
Wade Wegner <wade.wegner@gmail.com>
2016-08-06 15:29:53 -0500

    Fixed for async

--- src/clock/index.js
+++ src/clock/index.js
@@ -1,16 +1,22 @@
 var sleep = require('sleep');
+var requestify = require('requestify'); 
+var async = require('async');
 
-while (true){ //let's create a endless loop
+async.whilst(
+    function() { return true; },
+    function(callback) {
 
-	console.log('top');
-	var url = 'http://aninternetofbeers.com/trigger';
-	var requestify = require('requestify');
+		var url = 'https://aninternetofbeers.com/trigger';
+		requestify.get(url).then(function(response) {
+			console.log('response from trigger: ' + response.getBody());
+		});
 
-	console.log(url);
-	requestify.get(url).then(function(response) {
-		console.log('back');
-		console.log(response);
-	});
-
-    sleep.sleep (60);
-}
+        setTimeout(function() {
+            callback(null, true);
+        }, 60000);
+    },
+    function (err, n) {
+        // 5 seconds have passed, n = 5
+        console.log('err: ' + err);
+    }
+);
